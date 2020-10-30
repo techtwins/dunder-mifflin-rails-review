bf62735dcdc314f42f5fb78bc1c10d406b014a3f
techtwins <gshevaughn@gmail.com>
2020-10-30 15:59:30 -0400

    second commit, partner: David delaVega

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,26 +2,33 @@ class EmployeesController < ApplicationController
 
         
     def index
-
+        @employees = Employee.all
     end
 
     def show
-
+        @employee = Employee.find(params[:id])
     end
 
     def new
-
+        @employee = Employee.new
     end
 
     def create
-
+        @employee = Employee.create(employee_params)
+        redirect_to employee_path(@employee)
     end
 
     def edit
-
+        @employee = Employee.find(params[:id])
     end
 
     def update
         
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
     end
 end
